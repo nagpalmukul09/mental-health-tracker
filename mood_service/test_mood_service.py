b4d476449c2f557c32220c264d4db2226df650f7
nagpalmukul09 <nagpalmukul09@gmail.com>
2025-07-28 01:22:35 +0000

    Initial Commit of the microservices

--- mood_service/test_mood_service.py
+++ mood_service/test_mood_service.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from mood_service.main import app
+from main import app
 
 client = TestClient(app)
 
--- user_service/test_user_service.py
+++ user_service/test_user_service.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from user_service.main import app
+from main import app
 
 client = TestClient(app)
 
