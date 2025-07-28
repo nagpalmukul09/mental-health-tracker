91edc50229fac2de5584471d700edf51a3937a5c
nagpalmukul09 <nagpalmukul09@gmail.com>
2025-07-28 00:33:29 +0000

    Commit again

--- journal_service/test_journal_service.py
+++ journal_service/test_journal_service.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from journal_service.main import app
 
 client = TestClient(app)
 
--- recommendation_service/test_recommendation_service.py
+++ recommendation_service/test_recommendation_service.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from recommendation_service.main import app
 
 client = TestClient(app)
 
